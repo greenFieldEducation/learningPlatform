8afad0b0f97bfde21087d4ed698e8fe6a0933c2e
Alafajraoui <fullstackjs110299@gmail.com>
2024-07-02 15:35:56 +0200

    sequilize configuration and established connection

--- Backend/server/index.js
+++ Backend/server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 5000;
+const db=require("../DataBase/index.js")
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -14,7 +15,7 @@ app.use(express.urlencoded({extended:true}));
 
 
 app.listen(PORT, function () {
-    console.log("listening on port 3000!");
+    console.log("listening on port 5000!");
 });
 
 module.exports = app;
