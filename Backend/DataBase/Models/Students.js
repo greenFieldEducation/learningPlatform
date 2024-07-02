b767f375100be00a2600d54beb0a58daaf9a87a8
samir <samirabidi1123@gmail.com>
2024-07-02 19:10:29 +0100

    student creat and addstudent handel in the backend

--- Backend/DataBase/Models/Students.js
+++ Backend/DataBase/Models/Students.js
@@ -26,6 +26,9 @@ const student = sequelize.define("student", {
       Gender:{
         type: DataTypes.ENUM('Men', 'Women'),
         allowNull: false,
+    },PhoneNumber: {
+      type: DataTypes.STRING,
+      allowNull: false
     }
  });
  module.exports=student
