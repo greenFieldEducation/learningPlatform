8d4c39c92ae26d03fd3b69db80faf6af00c9b1a2
yosrjdly <yosrjdly@gmail.com>
2024-07-03 09:04:21 +0100

    navbar cpomponenet + styling using tailwind css + landing page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
-
+import { BrowserRouter as Router , Route , Routes } from 'react-router-dom'
+import LandingPage from '../src/pages/LandingPage.jsx'
 function App() {
  
 
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/sign-up-instructor" element={<SignUpInstructor />} />
+        <Route path="/sign-up-student" element={<SignUpStudent />} /> */}
+      </Routes>
+    </Router>
   )
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # learningPlatform
+
+
+
+#landingpage 
+ color palette : 
+    #f7f7f7  : white 
+    #d6d6d6 : light gray
+    #2a2b29 : strong gray 
+    #42a2b4 : blue 
+    #acafab : gray 
+    #000000 : black 
+
